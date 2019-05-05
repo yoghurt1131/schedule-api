89f801956a6d7b628193ee9bffa80d1231842b75
IshinFUKUOKA <yoghurt1131@gmail.com>
2019-05-05 10:41:38 +0900

    initialcommit

---
+++ src/main/kotlin/dev/yoghurt1131/calendarapi/CalendarApiApplication.kt
@@ -0,0 +1,11 @@
+package dev.yoghurt1131.calendarapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CalendarApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<CalendarApiApplication>(*args)
+}
---
+++ src/test/kotlin/dev/yoghurt1131/calendarapi/CalendarApiApplicationTests.kt
@@ -0,0 +1,16 @@
+package dev.yoghurt1131.calendarapi
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class CalendarApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
